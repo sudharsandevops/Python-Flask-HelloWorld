77e52f84789cbef6ef5ecfb3e4d867c49895fc2f
sudharsandevops <sudharsan.doodleblue@gmail.com>
2024-09-15 16:01:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ def hello_world():
 
 if __name__ == '__main__':
     # Bind to all network interfaces and use port 5000
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
